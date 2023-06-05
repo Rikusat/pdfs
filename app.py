a87447d051e6eb8d340dd35facd32714d44f5249
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-06 03:42:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
