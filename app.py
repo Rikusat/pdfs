04712e8ce51a9c19ae771d553af9307edba608f7
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-06-06 03:52:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
+
+# OpenAI APIキーを環境変数から取得
+api_key = os.environ['OPENAI_KEY']
+
+# OpenAI APIの初期化（仮定）
+openai.Api(api_key)
 
 
 def get_pdf_text(pdf_docs):
